c42a1b5a547aed5aab21588c2499b4f51b14fcb8
andrik2003 <andreykara11@gmail.com>
2022-05-30 20:17:14 +0300

    add colors and file functionality

--- logger.go
+++ logger.go
@@ -6,52 +6,143 @@ import (
 	"os"
 )
 
+var (
+	Black   = color("\033[1;30m%s\033[0m")
+	Red     = color("\033[1;31m%s\033[0m")
+	Green   = color("\033[1;32m%s\033[0m")
+	Yellow  = color("\033[1;33m%s\033[0m")
+	Blue    = color("\033[1;34m%s\033[0m")
+	Magenta = color("\033[1;35m%s\033[0m")
+	Teal    = color("\033[1;36m%s\033[0m")
+	White   = color("\033[1;37m%s\033[0m")
+)
+
 type customLogger struct {
 	CustomLogger
-	info  *log.Logger
-	warn  *log.Logger
-	error *log.Logger
+	info     *log.Logger
+	warn     *log.Logger
+	error    *log.Logger
+	debug    *log.Logger
+	tempData string
+}
+
+type customColors struct {
+	CustomColors
+	tempData string
+	prefix   string
+	color    func(...interface{}) string
 }
 
 type CustomLogger interface {
-	InfoLog(v ...interface{})
-	WarnLog(v ...interface{})
-	ErrorLog(v ...interface{})
+	Info(v ...interface{}) *customLogger
+	Warn(v ...interface{}) *customLogger
+	Error(v ...interface{}) *customLogger
+	Debug(v ...interface{}) *customLogger
+	FileLog(fileName string, v ...interface{})
+	File(file string) *customLogger
 }
 
-var instance *customLogger
-
-func NewCustomLogger() *customLogger {
-
-	instance = &customLogger{
-		info:  log.New(os.Stdout, "INFO: ", log.Ldate),
-		warn:  log.New(os.Stdout, "WARN: ", log.Ldate),
-		error: log.New(os.Stdout, "ERROR: ", log.Ldate),
-	}
-
-	return instance
+type CustomColors interface {
+	Prefix(color func(...interface{}) string, prefix string) *customColors
+	Console(v ...interface{}) *customLogger
 }
 
 var (
-	red    = color("\033[1;31m%s\033[0m")
-	yellow = color("\033[1;33m%s\033[0m")
-	teal   = color("\033[1;36m%s\033[0m")
+	loggerInstance *customLogger
+	colorsInstance *customColors
+	fileOs         *os.File
 )
 
-func (l *customLogger) InfoLog(v ...interface{}) {
-	l.info.Println(teal(v...))
+func NewCustomLogger() *customLogger {
+
+	loggerInstance = &customLogger{
+		info:  log.New(os.Stdout, Teal("INFO: "), log.Ldate),
+		warn:  log.New(os.Stdout, Yellow("WARN: "), log.Ldate),
+		error: log.New(os.Stdout, Red("ERROR: "), log.Ldate),
+		debug: log.New(os.Stdout, Green("DEBUG: "), log.Ldate),
+	}
+
+	return loggerInstance
 }
 
-func (l *customLogger) WarnLog(v ...interface{}) {
-	l.warn.Println(yellow(v...))
+func (l *customLogger) Info(v ...interface{}) *customLogger {
+
+	l.info.Println(v...)
+	l.tempData = fmt.Sprintf("%v", v)
+	return l
 }
 
-func (l *customLogger) ErrorLog(v ...interface{}) {
-	l.error.Println(red(v...))
+func (l *customLogger) Warn(v ...interface{}) *customLogger {
+	l.warn.Println(v...)
+	l.tempData = fmt.Sprintf("%v", v)
+	return l
 }
 
-func GetLogger() *customLogger {
-	return instance
+func (l *customLogger) Error(v ...interface{}) *customLogger {
+	l.error.Println(v...)
+	l.tempData = fmt.Sprintf("%v", v)
+	return l
+}
+
+func (l *customLogger) Debug(v ...interface{}) *customLogger {
+	l.debug.Println(v...)
+	l.tempData = fmt.Sprintf("%v", v)
+	return l
+}
+
+func NewCustomColorsLogger() *customColors {
+	colorsInstance = &customColors{}
+	return colorsInstance
+}
+
+func (c *customColors) Prefix(color func(...interface{}) string, prefix string) *customColors {
+	c.prefix = prefix
+	c.color = color
+	return c
+}
+
+func (c *customColors) Console(v ...interface{}) *customLogger {
+
+	logger := NewCustomLogger()
+	logger.tempData = fmt.Sprintf("%v", v)
+	log.New(os.Stdout, c.color(c.prefix), log.Ldate).Println(v...)
+
+	return logger
+}
+
+func (l *customLogger) File(file string) *customLogger {
+
+	// if we are logging in another file we are out from previous (close)
+	if fileOs != nil && fileOs.Name() != file {
+		fileOs.Close()
+	}
+
+	f, _ := os.OpenFile(file,
+		os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+
+	logger := log.New(f, "", log.LstdFlags)
+	logger.Println(fmt.Sprintf("%v", l.tempData))
+
+	fileOs = f
+	return l
+}
+
+func (l *customLogger) FileLog(file string, v ...interface{}) {
+
+	// if we are logging in another file we are out from previous (close)
+	if fileOs != nil && fileOs.Name() != file {
+		fileOs.Close()
+	}
+
+	f, _ := os.OpenFile(file,
+		os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+
+	defer f.Close()
+
+	fileOs = f
+	logger := log.New(f, "", log.LstdFlags)
+	logger.Println(fmt.Sprintf("%v", v))
+
 }
 
 func color(colorCode string) func(...interface{}) string {
